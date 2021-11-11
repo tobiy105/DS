fd8c9ee4e4b61868c6a6adb70c821660c6906ccc
tobiy105 <ed18o2s@leeds.ac.uk>
2021-11-11 01:48:03 +0000

    made template

--- app/nba/views.py
+++ app/nba/views.py
@@ -1,14 +1,31 @@
-from flask import render_template,session, request,redirect,url_for,flash
+from flask import Flask, render_template, session, request, redirect, url_for, flash
 from app import app
 from .forms import TeamSearchForm
+import json
+import requests
 
-#route for team search
-@app.route('/', methods=['GET','POST'])
+
+# route for team search
+@app.route('/', methods=['GET', 'POST'])
 def team():
     form = TeamSearchForm(request.form)
     if request.method == "POST":
         team = form.team.data
         flash(f'The team is being searched', 'success')
-        print(team)
-
+        return redirect(url_for('retrieved', teamAsked=team))
     return render_template('team.html', form=form)
+
+
+@app.route('/retrieved/<teamAsked>', methods=['GET', 'POST'])
+def retrieved(teamAsked):
+    request = requests.get('https://www.balldontlie.io/api/v1/teams')
+    teamsRetrieved = json.loads(request.content)
+    for finder in teamsRetrieved:
+        if finder['name'] == teamsAsked:
+            abbr = finder['abbreviation']
+            city = finder['city']
+            conf = finder['conference']
+            div = finder['division']
+            full = finder['full_name']
+            name = finder['name']
+    return render_template('retrieved.html', abbr=abbr, city=city, conf=conf, div=div, full=full, name=name)
