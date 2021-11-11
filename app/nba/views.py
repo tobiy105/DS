be0e002ae39e633507ec8326cae823f1fc1b660b
tobiy105 <ed18o2s@leeds.ac.uk>
2021-11-11 01:50:45 +0000

    made template for 2 page

--- app/nba/views.py
+++ app/nba/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, session, request, redirect, url_for, flash
 from app import app
 from .forms import TeamSearchForm
@@ -7,16 +8,32 @@ import requests
 
 # route for team search
 @app.route('/', methods=['GET', 'POST'])
+=======
+from flask import Flask,render_template,session, request,redirect,url_for,flash
+from app import app
+from .forms import TeamSearchForm
+import json
+
+#route for team search
+@app.route('/team', methods=['GET','POST'])
+>>>>>>> bdd3c24bb88b80228e6b7f7fef5eb0e81d0d94a3
 def team():
     form = TeamSearchForm(request.form)
     if request.method == "POST":
         team = form.team.data
         flash(f'The team is being searched', 'success')
+<<<<<<< HEAD
         return redirect(url_for('retrieved', teamAsked=team))
     return render_template('team.html', form=form)
 
 
 @app.route('/retrieved/<teamAsked>', methods=['GET', 'POST'])
+=======
+        return redirect(url_for('retrieved',teamAsked=team))
+    return render_template('team.html', form=form)
+    
+@app.route('/retrieved/<teamAsked>',methods=['GET','POST'])
+>>>>>>> bdd3c24bb88b80228e6b7f7fef5eb0e81d0d94a3
 def retrieved(teamAsked):
     request = requests.get('https://www.balldontlie.io/api/v1/teams')
     teamsRetrieved = json.loads(request.content)
@@ -28,4 +45,8 @@ def retrieved(teamAsked):
             div = finder['division']
             full = finder['full_name']
             name = finder['name']
+<<<<<<< HEAD
     return render_template('retrieved.html', abbr=abbr, city=city, conf=conf, div=div, full=full, name=name)
+=======
+    return render_template('retrieved.html',abbr=abbr,city=city,conf=conf,div=div,full=full,name=name)
+>>>>>>> bdd3c24bb88b80228e6b7f7fef5eb0e81d0d94a3
